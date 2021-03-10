38f4de85b96bedf2a81196a1aa027c9526bbce61
Aliaksei Dziubanau <dubell1986@gmail.com>
2021-03-11 00:36:07 +0300

    Update what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
+module.exports = function getSeason(date) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (!date) {
+  	return 'Unable to determine the time of year!';
+  }
+  if(date.getFullYear == undefined || date.getMonth() > 11){
+    throw new Error;
+  }
+
 };
