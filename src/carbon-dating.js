d3296a963e34057619a6ae4f5daf240ee099652d
Aliaksei Dziubanau <dubell1986@gmail.com>
2021-03-10 18:20:16 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -9,7 +9,7 @@ module.exports = function dateSample(sampleActivity) {
   if (!sampleActivity || typeof sampleActivity !== 'string' || arguments.length === 0 || isNaN(+sampleActivity) == true || +sampleActivity <= 0 || +sampleActivity > MODERN_ACTIVITY) {
   	return false;
   }
-  return Math.ceil(Math.log(MODERN_ACTIVITY / (+sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
-  //return MODERN_ACTIVITY;
-
+  else {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / (+sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
+  }
 };
